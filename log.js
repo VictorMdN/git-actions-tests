40f324ab56dc062de3017c761991c675f2f7f272
Victor Matheus <victormatheusdonascimento@gmail.com>
2026-03-29 04:45:50 -0300

    refactor log.js back to original

--- log.js
+++ log.js
@@ -1,10 +1,28 @@
-const credsRaw = process.env.GDRIVE_CREDENTIALS;
-
-console.log("Credenciais existem?", credsRaw);
-
-const credentials = JSON.parse(credsRaw);
+const { google } = require("googleapis");
 
 const auth = new google.auth.GoogleAuth({
-  credentials,
+  credentials: JSON.parse(process.env.GDRIVE_CREDENTIALS),
   scopes: ["https://www.googleapis.com/auth/spreadsheets"]
 });
+
+async function run() {
+
+  const client = await auth.getClient();
+
+  const sheets = google.sheets({ version: "v4", auth: client });
+
+  const agora = new Date().toISOString();
+
+  await sheets.spreadsheets.values.append({
+    spreadsheetId: process.env.SHEET_ID,
+    range: "A:A",
+    valueInputOption: "RAW",
+    requestBody: {
+      values: [[agora]]
+    }
+  });
+
+  console.log("Hora registrada:", agora);
+}
+
+run();
